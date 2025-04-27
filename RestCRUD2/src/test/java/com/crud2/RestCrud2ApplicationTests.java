b6b1de325044b6d98a5a9d3ba3b735741daf870e
thulasiramlal6@gmail.com <thulasiramlal6@gmail.com>
2025-04-27 12:55:12 +0530

    first time

---
+++ RestCRUD2/src/test/java/com/crud2/RestCrud2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestCrud2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
